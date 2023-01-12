2d29253ba33407e5cc00db5da7c53276c73629aa
FehPrado <pradofelipe20032002@gmail.com>
2023-01-11 22:10:44 -0300

    new pensamento

--- migrate/migrate.php
+++ migrate/migrate.php
@@ -63,5 +63,22 @@ echo "<\n>";
 
 
 
-
 $conect->close();
+
+$HOST = 'localhost';
+$USER = 'root';
+$PASS = '123456789';
+$DB = 'itinerary';
+
+//crinado conexão para o sedd
+$conect = new mysqli($HOST, $USER, $PASS, $DB);
+
+//fazendo o seed
+
+//products seed
+mysqli_query($conect, "INSERT INTO products (id, name) VALUES ('1', 'Felipe')");
+mysqli_query($conect, "INSERT INTO products (id, name) VALUES ('2', 'Murilo')");
+mysqli_query($conect, "INSERT INTO products (id, name) VALUES ('3', 'julio')");
+ 
+ //itinerary seed
+mysqli_query($conect, "INSERT INTO itinerary (id, name, description) VALUES ('1', 'Praia', 'Areia e agua')");
