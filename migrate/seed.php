deedb6b4fa72d207c6ab8c9c2c9c3bcb79f4075e
FehPrado <pradofelipe20032002@gmail.com>
2023-01-11 22:05:37 -0300

    correction

--- migrate/seed.php
+++ migrate/seed.php
@@ -2,20 +2,18 @@
 $HOST = 'localhost';
 $USER = 'root';
 $PASS = '123456789';
-
+$DB = 'itinerary';
 
 //crinado conexão para o sedd
-$conect = new mysqli($HOST, $USER, $PASS);
-
+$conect = new mysqli($HOST, $USER, $PASS, $DB);
 
 //fazendo o seed
 
 //products seed
-
-mysqli_query($conect, "INSERT INTO 'itinerary'.'products' ('id', 'name') VALUES ('1', 'Felipe')");
-mysqli_query($conect, "INSERT INTO 'itinerary'.'products' ('id', 'name') VALUES ('2', 'Murilo')");
-mysqli_query($conect, "INSERT INTO 'itinerary'.'products' ('id', 'name') VALUES ('3', 'julio')");
+mysqli_query($conect, "INSERT INTO products (id, name) VALUES ('1', 'Felipe')");
+mysqli_query($conect, "INSERT INTO products (id, name) VALUES ('2', 'Murilo')");
+mysqli_query($conect, "INSERT INTO products (id, name) VALUES ('3', 'julio')");
  
  //itinerary seed
-mysqli_query($conect, "INSERT INTO 'itinerary'.'itinerary' ('id', 'name', 'description') VALUES ('1', 'Praia', 'Areia e agua')");
+mysqli_query($conect, "INSERT INTO itinerary (id, name, description) VALUES ('1', 'Praia', 'Areia e agua')");
 
